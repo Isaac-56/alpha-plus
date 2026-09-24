85566aae591e00ef23770855038c5eaff141af98
Yeshak Tsegaye <yeshak34@gmail.com>
2026-09-24 15:43:31 +0300

    Stabilize vehicle category picker test

--- test/service_registration_screen_test.dart
+++ test/service_registration_screen_test.dart
@@ -112,7 +112,11 @@ void main() {
     expect(find.text('SUV / 4x4'), findsOneWidget);
     expect(find.text('Boda boda (motorcycle)'), findsOneWidget);
     expect(find.text('Bajaj / Tuk-tuk (three-wheeler)'), findsOneWidget);
-    await tester.scrollUntilVisible(find.text('Scooter'), 120);
+    await tester.scrollUntilVisible(
+      find.text('Scooter'),
+      120,
+      scrollable: find.byType(ListView).last,
+    );
     expect(find.text('Scooter'), findsOneWidget);
     expect(find.text('Standard'), findsNothing);
     expect(find.text('Comfort'), findsNothing);
